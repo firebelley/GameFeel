03848088f84b016fafd97f5948e5a16c04ce34f3
firebelley <firebelley@users.noreply.github.com>
2019-07-14 19:37:20 -0400

    update cursor

--- scripts/Resources/Cursor.cs
+++ scripts/Resources/Cursor.cs
@@ -4,14 +4,24 @@ namespace GameFeel.Resources
 {
     public class Cursor : Sprite
     {
+        private const string ANIM_DEFAULT = "default";
+
+        private AnimationPlayer _animationPlayer;
+
         public override void _Ready()
         {
             Input.SetMouseMode(Input.MouseMode.Hidden);
+            _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
         }
 
         public override void _Process(float delta)
         {
             GlobalPosition = GetGlobalMousePosition();
+            if (Input.IsActionJustPressed("attack"))
+            {
+                _animationPlayer.Stop();
+                _animationPlayer.Play(ANIM_DEFAULT);
+            }
         }
     }
 }

2d744689767b3581d4a74255b4dc40a6f17b3920
firebelley <firebelley@users.noreply.github.com>
2019-07-22 19:46:02 -0400

    basic spider attacking

--- scripts/GameObject/Spider.cs
+++ scripts/GameObject/Spider.cs
@@ -8,10 +8,14 @@ namespace GameFeel.GameObject
     public class Spider : KinematicBody2D
     {
         private const string ANIM_RUN = "run";
+        private const string ANIM_ATTACK = "attack";
+        private const string ANIM_IDLE = "idle";
+        private const float ATTACK_RANGE = 40f;
 
         private AnimatedSprite _animatedSprite;
         private AnimationPlayer _animationPlayer;
         private ResourcePreloader _resourcePreloader;
+        private Particles2D _attackIntentParticles;
 
         private HealthComponent _healthComponent;
         private PathfindComponent _pathfindComponent;
@@ -21,23 +25,29 @@ namespace GameFeel.GameObject
         private enum State
         {
             PURSUE,
-            SPAWNING
+            SPAWN,
+            ATTACK,
+            ATTACK_PREPARATION
         }
 
         public override void _Ready()
         {
             _stateMachine.AddState(State.PURSUE, StatePursue);
-            _stateMachine.AddState(State.SPAWNING, StateSpawning);
-            _stateMachine.SetInitialState(State.SPAWNING);
+            _stateMachine.AddState(State.SPAWN, StateSpawning);
+            _stateMachine.AddState(State.ATTACK, StateAttack);
+            _stateMachine.AddState(State.ATTACK_PREPARATION, StateAttackPreparation);
+            _stateMachine.SetInitialState(State.SPAWN);
 
             _animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
             _resourcePreloader = GetNode<ResourcePreloader>("ResourcePreloader");
             _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+            _attackIntentParticles = GetNode<Particles2D>("AttackIntentParticles");
 
             _healthComponent = GetNode<HealthComponent>("HealthComponent");
             _pathfindComponent = GetNode<PathfindComponent>("PathfindComponent");
 
             _healthComponent.Connect(nameof(HealthComponent.HealthDepleted), this, nameof(OnHealthDepleted));
+            _animatedSprite.Connect("animation_finished", this, nameof(OnAnimationFinished));
         }
 
         public override void _Process(float delta)
@@ -76,6 +86,39 @@ namespace GameFeel.GameObject
             {
                 _animatedSprite.FlipH = false;
             }
+
+            var player = GetTree().GetFirstNodeInGroup<Player>(Player.GROUP);
+            if (player != null)
+            {
+                if (player.GlobalPosition.DistanceSquaredTo(GlobalPosition) < ATTACK_RANGE * ATTACK_RANGE)
+                {
+                    _stateMachine.ChangeState(State.ATTACK_PREPARATION);
+                }
+            }
+        }
+
+        private void StateAttackPreparation(bool isStateNew)
+        {
+            if (isStateNew)
+            {
+                _attackIntentParticles.Restart();
+                _attackIntentParticles.Emitting = true;
+                _animatedSprite.Play(ANIM_IDLE);
+            }
+
+            if (!_attackIntentParticles.IsEmitting())
+            {
+                _stateMachine.ChangeState(State.ATTACK);
+            }
+        }
+
+        private void StateAttack(bool isStateNew)
+        {
+            if (isStateNew)
+            {
+                _animatedSprite.Play(ANIM_ATTACK);
+
+            }
         }
 
         private void Kill()
@@ -89,6 +132,18 @@ namespace GameFeel.GameObject
         private void OnHealthDepleted()
         {
             Kill();
+        }
+
+        private void OnAnimationFinished()
+        {
+            if (_animatedSprite.Animation == ANIM_ATTACK)
+            {
+                _stateMachine.ChangeState(State.PURSUE);
+                var ball = _resourcePreloader.InstanceScene<RigidBody2D>("SpiderBall");
+                GameZone.EffectsLayer.AddChild(ball);
+                ball.GlobalPosition = GlobalPosition - new Vector2(0, 6f);
+                ball.LinearVelocity = (GetTree().GetFirstNodeInGroup<Player>(Player.GROUP).GlobalPosition - GlobalPosition).Normalized() * 100f;
+            }
         }
     }
 }

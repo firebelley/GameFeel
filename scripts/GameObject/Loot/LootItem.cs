6cd4362024ae5236ee9a8b300841ac8bdfa1235a
firebelley <firebelley@users.noreply.github.com>
2019-08-04 13:38:27 -0400

    make loot items disappear

--- scripts/GameObject/Loot/LootItem.cs
+++ scripts/GameObject/Loot/LootItem.cs
@@ -4,7 +4,12 @@ namespace GameFeel.GameObject.Loot
 {
     public class LootItem : KinematicBody2D
     {
-        private string ANIM_IDLE = "idle";
+        private const string GROUP = "loot_item";
+        private const string ANIM_IDLE = "idle";
+        private const string ANIM_DEFAULT = "default";
+
+        private const float SEPARATION_DISTANCE = 15f;
+        private const float SEPARATION_SPEED = 15f;
 
         private const float MIN_ANGLE = 90f - 15f;
         private const float MAX_ANGLE = 90f + 15f;
@@ -17,6 +22,8 @@ namespace GameFeel.GameObject.Loot
 
         private AnimationPlayer _animationPlayer;
         private CollisionShape2D _collisionShape2d;
+        private AnimationPlayer _blinkAnimationPlayer;
+        private Timer _deathTimer;
 
         private float _floorY;
         private Vector2 _velocity;
@@ -26,18 +33,35 @@ namespace GameFeel.GameObject.Loot
         {
             _collisionShape2d = GetNode<CollisionShape2D>("CollisionShape2D");
             _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+            _blinkAnimationPlayer = GetNode<AnimationPlayer>("BlinkAnimationPlayer");
+            _deathTimer = GetNode<Timer>("DeathTimer");
             _velocity = Vector2.Right.Rotated(Mathf.Deg2Rad(Main.RNG.RandfRange(MIN_ANGLE, MAX_ANGLE)));
             _velocity *= Main.RNG.RandfRange(MIN_SPEED, MAX_SPEED);
+            AddToGroup(GROUP);
+
+            _deathTimer.Connect("timeout", this, nameof(OnDeathTimerTimeout));
         }
 
         public override void _Process(float delta)
+        {
+            if (_bounces <= MAX_BOUNCES)
+            {
+                Bounce();
+            }
+            else
+            {
+                Separate();
+            }
+        }
+
+        private void Bounce()
         {
             if (_floorY == 0f)
             {
                 _floorY = GlobalPosition.y;
             }
 
-            _velocity += Vector2.Down * GRAVITY * delta;
+            _velocity += Vector2.Down * GRAVITY * GetProcessDeltaTime();
             if (_velocity.y > 0f && GlobalPosition.y >= _floorY)
             {
                 _bounces++;
@@ -45,10 +69,31 @@ namespace GameFeel.GameObject.Loot
             }
 
             _velocity = MoveAndSlide(_velocity);
+        }
 
-            if (_bounces == MAX_BOUNCES)
+        private void Separate()
+        {
+            var pushVec = Vector2.Zero;
+            foreach (var node in GetTree().GetNodesInGroup(GROUP))
+            {
+                if (node is Node2D n)
+                {
+                    if (GlobalPosition.DistanceSquaredTo(n.GlobalPosition) < SEPARATION_DISTANCE * SEPARATION_DISTANCE)
+                    {
+                        pushVec = (GlobalPosition - n.GlobalPosition);
+                        break;
+                    }
+                }
+            }
+
+            if (pushVec == Vector2.Zero)
             {
                 Disable();
+            }
+            else
+            {
+                pushVec = pushVec.Normalized() * SEPARATION_SPEED;
+                _velocity = MoveAndSlide(pushVec);
             }
         }
 
@@ -61,6 +106,12 @@ namespace GameFeel.GameObject.Loot
             SetProcess(false);
             _animationPlayer.Play(ANIM_IDLE);
             _collisionShape2d.Disabled = true;
+            _deathTimer.Start();
+        }
+
+        private void OnDeathTimerTimeout()
+        {
+            _blinkAnimationPlayer.Play(ANIM_DEFAULT);
         }
     }
 }

8ea1621e4717d97873787adf843fe4c0d7573843
firebelley <firebelley@users.noreply.github.com>
2019-10-02 19:33:54 -0400

    remove gd.print

--- scripts/GameObject/Loot/Equipment.cs
+++ scripts/GameObject/Loot/Equipment.cs
@@ -23,7 +23,6 @@ namespace GameFeel.GameObject.Loot
 
         public override void _Ready()
         {
-            GD.Print(GetTree().GetNodesInGroup(Player.GROUP).Count);
             var player = GetTree().GetFirstNodeInGroup<Player>(Player.GROUP);
             if (player != null)
             {
